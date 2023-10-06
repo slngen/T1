84bb55dce22c5a0f3ea9d95eaec14120d1fbdc11
CT <slngen@163.com>
2023-10-06 03:58:45 +0000

    Design Backbone A&B

--- Code/Pytorch-chain/BackboneA.py
+++ Code/Pytorch-chain/BackboneA.py
@@ -76,7 +76,6 @@ class Backbone(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_2),
         )
-
         self.encoder_2_1_3 = nn.Sequential(
             nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
@@ -101,7 +100,6 @@ class Backbone(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_2),
         )
-
         self.encoder_2_2_3 = nn.Sequential(
             nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
@@ -139,23 +137,6 @@ class Backbone(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_1),
         )
-        self.decoder_2_0_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
-        self.decoder_2_0_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
-
         self.decoder_2_1_3 = nn.Sequential(
             nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
@@ -164,23 +145,6 @@ class Backbone(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_1),
         )
-        self.decoder_2_1_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
-        self.decoder_2_1_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
-
         self.decoder_2_2_3 = nn.Sequential(
             nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
@@ -189,92 +153,26 @@ class Backbone(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_1),
         )
-        self.decoder_2_2_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
-        self.decoder_2_2_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_2, out_channels=self.dim_1, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_1),
-        )
 
         # layer-~1
-        self.decoder_1_0_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=3, stride=1, padding=1),
+        self.decoder_1_0_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_0),
             nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_0),
         )
-        self.decoder_1_0_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=5, stride=1, padding=2),
+        self.decoder_1_1_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_0),
             nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_0),
         )
-        self.decoder_1_0_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-
-        self.decoder_1_1_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-        self.decoder_1_1_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-        self.decoder_1_1_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-
-        self.decoder_1_2_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-        self.decoder_1_2_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-            nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=self.dim_0),
-        )
-        self.decoder_1_2_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=7, stride=1, padding=3),
+        self.decoder_1_2_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_1, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=self.dim_0),
             nn.Conv2d(in_channels=self.dim_0, out_channels=self.dim_0, kernel_size=1, stride=1, padding=0),
@@ -283,74 +181,24 @@ class Backbone(nn.Module):
         )
 
         # layer-~0
-        self.decoder_0_0_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=3, stride=1, padding=1),
+        self.decoder_0_0_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=2),
             nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=2),
         )
-        self.decoder_0_0_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=5, stride=1, padding=2),
+        self.decoder_0_1_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=2),
             nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=2),
         )
-        self.decoder_0_0_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-
-        self.decoder_0_1_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-        self.decoder_0_1_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-        self.decoder_0_1_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=7, stride=1, padding=3),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-
-        self.decoder_0_2_3 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-        self.decoder_0_2_5 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-            nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.BatchNorm2d(num_features=2),
-        )
-        self.decoder_0_2_7 = nn.Sequential(
-            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=7, stride=1, padding=3),
+        self.decoder_0_2_1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.dim_0, out_channels=2, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=2),
             nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1, padding=0),
@@ -379,59 +227,37 @@ class Backbone(nn.Module):
         f_2_2_3 = self.encoder_2_2_3(f_1_3)
         f_2_2_5 = self.encoder_2_2_5(f_1_5)
         f_2_2_7 = self.encoder_2_2_7(f_1_7)
+        f_2_3_3 = self.encoder_2_3_3(f_1_3)
+        f_2_3_5 = self.encoder_2_3_5(f_1_5)
+        f_2_3_7 = self.encoder_2_3_7(f_1_7)
 
         '''
         Decoder
         '''
         ## layer-~2
         d_2_0_3 = self.decoder_2_0_3(f_2_0_3+f_2_0_5+f_2_0_7)
-        # d_2_0_5 = self.decoder_2_0_5(f_2_0_5)
-        # d_2_0_7 = self.decoder_2_0_7(f_2_0_7)
-        d_2_1_3 = self.decoder_2_0_3(f_2_1_3+f_2_1_5+f_2_1_7)
-        # d_2_1_5 = self.decoder_2_0_5(f_2_1_5)
-        # d_2_1_7 = self.decoder_2_0_7(f_2_1_7)
-        d_2_2_3 = self.decoder_2_0_3(f_2_2_3+f_2_2_5+f_2_2_7)
-        # d_2_2_5 = self.decoder_2_0_5(f_2_2_5)
-        # d_2_2_7 = self.decoder_2_0_7(f_2_2_7)
+        d_2_1_3 = self.decoder_2_1_3(f_2_1_3+f_2_1_5+f_2_1_7)
+        d_2_2_3 = self.decoder_2_2_3(f_2_2_3+f_2_2_5+f_2_2_7)
 
         ## layer-~1
-        d_1_0_3 = self.decoder_1_0_3(d_2_0_3+f_1_3+f_1_5+f_1_7)
-        # d_1_0_5 = self.decoder_1_0_5(d_2_0_5+f_1_5)
-        # d_1_0_7 = self.decoder_1_0_7(d_2_0_7+f_1_7)
-        d_1_1_3 = self.decoder_1_0_3(d_2_1_3+f_1_3+f_1_5+f_1_5)
-        # d_1_1_5 = self.decoder_1_0_5(d_2_1_5+f_1_5)
-        # d_1_1_7 = self.decoder_1_0_7(d_2_1_7+f_1_7)
-        d_1_2_3 = self.decoder_1_0_3(d_2_2_3+f_1_3+f_1_5+f_1_7)
-        # d_1_2_5 = self.decoder_1_0_5(d_2_2_5+f_1_5)
-        # d_1_2_7 = self.decoder_1_0_7(d_2_2_7+f_1_7)
+        d_1_0_1 = self.decoder_1_0_1(d_2_0_3+f_1_3+f_1_5+f_1_7)
+        d_1_1_1 = self.decoder_1_1_1(d_2_1_3+f_1_3+f_1_5+f_1_7)
+        d_1_2_1 = self.decoder_1_2_1(d_2_2_3+f_1_3+f_1_5+f_1_5)
 
         ## layer-~0
-        out_0_3 = self.decoder_0_0_3(d_1_0_3+f_0)
-        # out_0_5 = self.decoder_0_0_5(d_1_0_5+f_0)
-        # out_0_7 = self.decoder_0_0_7(d_1_0_7+f_0)
-        out_1_3 = self.decoder_0_0_3(d_1_1_3+f_0)
-        # out_1_5 = self.decoder_0_0_5(d_1_1_5+f_0)
-        # out_1_7 = self.decoder_0_0_7(d_1_1_7+f_0)
-        out_2_3 = self.decoder_0_0_3(d_1_2_3+f_0)
-        # out_2_5 = self.decoder_0_0_5(d_1_2_5+f_0)
-        # out_2_7 = self.decoder_0_0_7(d_1_2_7+f_0)
+        out_0_0_1 = self.decoder_0_0_1(d_1_0_1+f_0)
+        out_0_1_1 = self.decoder_0_1_1(d_1_1_1+f_0)
+        out_0_2_1 = self.decoder_0_2_1(d_1_2_1+f_0)
 
-        out_0_3 = self.softmax(out_0_3)
-        # out_0_5 = self.softmax(out_0_5)
-        # out_0_7 = self.softmax(out_0_7)
-        out_1_3 = self.softmax(out_1_3)
-        # out_1_5 = self.softmax(out_1_5)
-        # out_1_7 = self.softmax(out_1_7)
-        out_2_3 = self.softmax(out_2_3)
-        # out_2_5 = self.softmax(out_2_5)
-        # out_2_7 = self.softmax(out_2_7)
+        out_0_1 = self.softmax(out_0_0_1)
+        out_1_1 = self.softmax(out_0_1_1)
+        out_2_1 = self.softmax(out_0_2_1)
 
-        # score = out_0_3+out_0_5+out_0_7+out_1_3+out_1_5+out_1_7+out_2_3+out_2_5+out_2_7
-        score = out_0_3+out_1_3+out_2_3
+        score = out_0_1+out_1_1+out_2_1
         score = self.softmax(score)
         return [score]
     
 if __name__ == '__main__':
     model = Backbone()
     # print(model)
-    summary(model, input_size=(8, config.input_dim, config.image_size, config.image_size))
+    summary(model, input_size=(32, config.input_dim, config.image_size, config.image_size))
--- Code/Pytorch-chain/Config.py
+++ Code/Pytorch-chain/Config.py
@@ -7,7 +7,7 @@ LastEditTime: 2023-09-13 11:05
 from easydict import EasyDict as ed
 
 config = ed({
-    "device":"cuda:0",  # CPU or cuda
+    "device":"cuda:1",  # CPU or cuda
     "class_nums":2,
     # Task
     "task_flag_Dict":{
@@ -61,7 +61,7 @@ config = ed({
     "lr_end":5e-5,
     "warmup_epochs":0,
     # Backbone
-    "backbone_type": "chain-B",
+    "backbone_type": "chain-A",
     "input_dim":6,
     "backbone_dims":[32, 64, 128]
 })
